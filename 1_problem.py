7211bd7dcae30f4de3f9433a3a3328fc4cd1e074
kjy <kjy5947@gmail.com>
2021-03-03 13:57:36 +0900

    Updating contents

--- 1_problem.py
+++ 1_problem.py
@@ -1,2 +1,14 @@
-n,k=map(int,input().split())
-a=list(map(int,input().split()))
+
+a,b=map(int,input().split())
+
+cnt=0
+str=0
+for i in range(1,a+1):
+    if(a%i==0):
+        cnt+=1
+        if(cnt==b):
+            str=i
+            print(str)
+            break
+if(cnt<b):
+    print(-1)
--- prc.py
+++ prc.py
@@ -1,17 +1,17 @@
-num=int(input())
-
-
-for i in range(num):
-    arr=input()
-    arr=arr.upper()
-    size=len(arr)
-    chk = 0
-    for j in range((size//2)):
-        if (arr[j] == arr[(size - 1) - j]):
-            continue
-        else:
-            chk = 1
-            print("#%d NO" %(i+1))
-            break
-    if(chk==0):
-        print("#%d YES" %(i+1))
+arr=[list(map(int,input().split())) for _ in range(7)]
+size=len(arr)-1
+cnt2=0
+for i in range(7):
+    for j in range(3):
+        cnt0 = 0
+        cnt1 = 0
+        for k in range(j,j+(5 // 2)):
+            if (arr[i][k] == arr[i][size - k]):
+                cnt0 += 1
+            if (arr[k][i] == arr[size-k][i]):
+                cnt1 += 1
+        if (cnt0 == 2):
+            cnt2 += 1
+        if (cnt1 == 2):
+            cnt2 += 1
+print(cnt2)
